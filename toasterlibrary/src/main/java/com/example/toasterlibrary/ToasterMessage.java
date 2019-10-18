8b1036f1b6e14e4323fd89417142772719f82eda
Jasmine Jeyakani Soundarya <jasmine@epagemaker.com>
2019-10-18 14:00:36 +0530

    Publish Toast show

--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 public class ToasterMessage {
 
     public static void s(Context c, String message) {
-        Toast.makeText(c, message, Toast.LENGTH_LONG);
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show();
 
     }
 }
